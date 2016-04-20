f2921332e664b27d946b51de78d653148e990309
Mijea Mihai <mmihai@suitsupply.com>
2016-04-20 22:53:55 +0200

    add Fraction methods

--- fraction.py
+++ fraction.py
@@ -4,3 +4,25 @@ class Fraction:
 
         self.num = top
         self.den = bottom
+
+
+    def show(self):
+        print(self.num, '/', self.den)
+
+    def __str__(self):
+        return str(self.num)+"/"+str(self.den)
+
+
+    def __add__(self, other):
+        newnum = self.num * other.den + other.num * self.den
+        newden = self.den * other.den
+        return Fraction(newnum, newden)
+
+myF = Fraction(2,3)
+myF.show()
+print(myF)
+
+f1 = Fraction(1,4)
+f2 = Fraction(1,2)
+f3 = f1+f2
+print( f3)

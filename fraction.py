35fe0100600464a7de5961c356ca2addaae621ce
Mijea Mihai <mmihai@suitsupply.com>
2016-04-23 13:34:57 +0200

    eq and gcd for add method

--- fraction.py
+++ fraction.py
@@ -16,13 +16,35 @@ class Fraction:
     def __add__(self, other):
         newnum = self.num * other.den + other.num * self.den
         newden = self.den * other.den
-        return Fraction(newnum, newden)
+        commonDivisor = gcd(newnum, newden)
+        return Fraction(newnum//commonDivisor, newden//commonDivisor)
+
+    def __eq__(self, other):
+        firstnum = self.num * other.den
+        secondnum = self.den * other.num
+
+        return firstnum == secondnum
+
+#HELPER greatest common divisor
+
+def gcd(m,n):
+    while m%n !=0:
+        oldm = m
+        oldn = n
+
+        m = oldn
+        n = oldm%oldn
+
+    return n
+
+
 
 myF = Fraction(2,3)
 myF.show()
 print(myF)
 
-f1 = Fraction(1,4)
+f1 = Fraction(2,4)
 f2 = Fraction(1,2)
 f3 = f1+f2
 print( f3)
+print( f1 == f2)

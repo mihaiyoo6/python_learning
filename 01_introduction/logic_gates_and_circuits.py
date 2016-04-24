2b2fbc8bff39589cebf0286ff6d41c809b964d9a
Mijea Mihai <mmihai@suitsupply.com>
2016-04-24 10:33:34 +0200

    change folder structure

---
+++ 01_introduction/logic_gates_and_circuits.py
@@ -0,0 +1,132 @@
+class LogicGate:
+
+    def __init__(self, n):
+        self.label = n
+        self.output = None
+
+    def get_label(self):
+        return self.label
+
+    def get_output(self):
+        self.output = self.perform_gate_logic()
+        return self.output
+
+
+class BinaryGate(LogicGate):
+
+    def __init__(self, n):
+        LogicGate.__init__(self, n)
+
+        self.pinA = None
+        self.pinB = None
+
+    def get_pin_a(self):
+        if self.pinA == None:
+            return int(input("Enter Pin A input for gate " + self.get_label() + "--> "))
+        else:
+            return self.pinA.get_from().get_output()
+
+    def get_pin_b(self):
+        if self.pinB == None:
+            return int(input("Enter Pin B input for gate " + self.get_label() + "--> "))
+        else:
+            return self.pinB.get_from().get_output()
+
+    def set_next_pin(self, source):
+        if self.pinA == None:
+            self.pinA = source
+        else:
+            if self.pinB == None:
+                self.pinB = source
+            else:
+                print("Cannot Connect: NO EMPTY PINS on this gate")
+
+
+class UnaryGate(LogicGate):
+
+    def __init__(self, n):
+        LogicGate.__init__(self, n)
+
+        self.pin = None
+
+    def get_pin(self):
+        if self.pin == None:
+            return int(input("Enter Pin input for gate " + self.get_label() + "--> "))
+        else:
+            return self.pin.get_from().get_output()
+
+    def set_next_pin(self, source):
+        if self.pin == None:
+            self.pin = source
+        else:
+            print("Cannot Connect: NO EMPTY PINS on this gate")
+
+
+class AndGate(BinaryGate):
+
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+
+    def perform_gate_logic(self):
+        a = self.get_pin_a()
+        b = self.get_pin_b()
+        if a == 1 and b == 1:
+            return 1
+        else:
+            return 0
+
+
+class OrGate(BinaryGate):
+
+    def __init__(self, n):
+        BinaryGate.__init__(self, n)
+
+    def perform_gate_logic(self):
+
+        a = self.get_pin_a()
+        b = self.get_pin_b()
+        if a == 1 or b == 1:
+            return 1
+        else:
+            return 0
+
+
+class NotGate(UnaryGate):
+
+    def __init__(self, n):
+        UnaryGate.__init__(self, n)
+
+    def perform_gate_logic(self):
+        if self.get_pin():
+            return 0
+        else:
+            return 1
+
+
+class Connector:
+
+    def __init__(self, f_gate, t_gate):
+        self.from_gate = f_gate
+        self.to_gate = t_gate
+
+        t_gate.set_next_pin(self)
+
+    def get_from(self):
+        return self.from_gate
+
+    def get_to(self):
+        return self.to_gate
+
+
+def main():
+    g1 = AndGate("G1")
+    g2 = AndGate("G2")
+    g3 = OrGate("G3")
+    g4 = NotGate("G4")
+    c1 = Connector(g1, g3)
+    c2 = Connector(g2, g3)
+    c3 = Connector(g3, g4)
+
+    print(g4.get_output())
+
+main()
